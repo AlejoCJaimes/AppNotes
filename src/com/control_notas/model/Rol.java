b3e5c1b6e40a6b62fabc23ff877d8a1951fb836f
Alejandro Jaimes <juanalejo2010@gmail.com>
2021-05-08 12:13:24 -0500

    Creación de Clases
    Se mapean las primeras dos clases
    de la base de datos 
    - __Rol__
    - __Usuario__
    
    Id: #2

---
+++ src/com/control_notas/model/Rol.java
@@ -0,0 +1,44 @@
+/**
+ * Clase Rol.
+ * 
+*/
+package com.control_notas.model;
+
+public class Rol {
+    
+    //Atributos
+    
+    int idRol;
+    String rol;
+    
+    //Constructor por defecto
+    
+    public Rol () {};
+
+    public Rol(int idRol, String rol) {
+        this.idRol = idRol;
+        this.rol = rol;
+    }
+    
+    //Comentario de Prueba
+    
+    //Comentario de prueba VOLKMAR
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
+}
---
+++ src/com/control_notas/model/Usuario.java
@@ -0,0 +1,85 @@
+/**
+ * Clase Usuario.
+ * 
+*/
+package com.control_notas.model;
+
+public class Usuario extends Rol {
+    
+    //Atributos
+    int idUsuario;
+    String correo, clave, fechaCreacion;
+    int status;
+    
+    //Constructor por defecto
+    public Usuario () {};
+    
+    //Constructor con parametros.
+
+        public Usuario(int idUsuario, String correo, String clave, String fechaCreacion, int status, int idRol, String rol) {
+        super(idRol, rol);
+        this.idUsuario = idUsuario;
+        this.correo = correo;
+        this.clave = clave;
+        this.fechaCreacion = fechaCreacion;
+        this.status = status;
+    }
+    
+    //Getters and Setters
+
+    public int getIdUsuario() {
+        return idUsuario;
+        //Comentario en getIdUsuario()
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    //Funciones 
+    
+    //Encriptar contraseña
+    
+    private String encriptarPassword()
+    {
+        //proceso
+        return "Password";
+       
+    }
+   
+    
+    
+    
+}
